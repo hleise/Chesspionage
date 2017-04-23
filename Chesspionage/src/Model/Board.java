a019ae0297e1ae910753db0115b7f4dc50cae9c8
r1413 <raydunc15@gmail.com>
2017-04-22 22:35:29 -0600

    Added base code to Square class Implemented setupBoard method in Board class. Test code pending Started PieceFactory...will revisit in the future

--- Chesspionage/src/Model/Board.java
+++ Chesspionage/src/Model/Board.java
@@ -18,9 +18,54 @@ public class Board {
     }
 
     //Methods
-    public boolean setupBoard(int player){
+    public boolean setupBoard(int player, int[] pieceRank, int[] pieceFile){
+        /*
+        Takes as input the player number and pieceRank and pieceFile arrays
+
+        pieceRank and pieceFile are each length 16 arrays which contain the index of it's location.
+        Piece identity is by index: Pawn[0-7], Rook[8-9], Knight[10-11], Bishop[12-13], Queen[14], King[15]
+
+        Returns true on success and false on failure and when pieces overlap or have invalid rank
+         */
         if(boardSetup) return false;
         //Set board pieces
+        int rank,file;
+        PieceColor pieceColor = null;
+        if(player == 0) {
+            pieceColor = PieceColor.LIGHT;
+        } else {
+            pieceColor = PieceColor.DARK;
+        }
+
+        PieceType pieceType = PieceType.PAWN;
+        for(int i = 0; i < 16; i++){
+            rank = pieceRank[0];
+            file = pieceFile[0];
+            if(squares[8*rank + file].getPiece() != null){
+                //If there is already a piece in the location in question
+                return false;
+            }
+            switch(i){
+                case 8|9:
+                    pieceType = PieceType.ROOK;
+                    break;
+                case 10|11:
+                    pieceType = PieceType.KNIGHT;
+                    break;
+                case 12|13:
+                    pieceType = PieceType.BISHOP;
+                    break;
+                case 14:
+                    pieceType = PieceType.QUEEN;
+                    break;
+                case 15:
+                    pieceType = PieceType.KING;
+                    break;
+            }
+            Piece newPiece = new Piece(pieceColor, pieceType);
+            pieceSet[player*16 + i] = newPiece;
+            squares[8*rank + file].setPiece(newPiece);
+        }
         boardSetup = true;
         return true;
     }
---
+++ Chesspionage/src/Model/PieceFactory.java
@@ -0,0 +1,11 @@
+package Model;
+
+/**
+ * Created by Raymond on 4/22/17.
+ */
+public class PieceFactory {
+    public Piece[] createPieces(PieceType pieceType, int numPieces){
+        Piece[] pieces = new Piece[numPieces];
+        return pieces;
+    }
+}
